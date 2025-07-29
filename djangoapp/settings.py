79bd485834367ad1ee8e67491cbc70d25ebed33c
sahilcmd3 <msgsahil5@gmail.com>
2025-07-30 02:38:00 +0530

    updates

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-devel
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['49.36.239.208']
 
 
 # Application definition
